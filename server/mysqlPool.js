6aec86cfdffed5204ef39c5ee67637d10b22305f
Gumimaco <matus.martiska@gmail.com>
2023-01-03 22:44:30 +0100

    changed mySQL connection

--- server/mysqlPool.js
+++ server/mysqlPool.js
@@ -3,7 +3,7 @@ var mysql = require("mysql2");
 var pool = mysql.createPool({
   host: 'containers-us-west-115.railway.app',
   user: 'root',
-  password: 'UPPMN5e5l4lByVyldnfd',
+  password: 'H9tntREQPSj7vFEK4ygp',
   database: 'railway',
   port: 5819,
   // host: 'localhost',
